817ec7c6600a735c8afa5a86ddac959425595a49
Fadil Andrian <akhsanfadil@gmail.com>
2026-01-19 18:02:23 +0700

    Adding Product API

--- internal/product/model.go
+++ internal/product/model.go
@@ -7,6 +7,12 @@ type Product struct {
 	Name      string
 	Price     int
 	Stock     int
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type ProductRequest struct {
+	Name  string `validate:"required,min=3"`
+	Price int    `validate:"required,gt=0"`
+	Stock int    `validate:"required,gte=0"`
 }
